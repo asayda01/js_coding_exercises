c07f418cf11eaa3640182ce94cd42f95451ab78c
Cem Saydam <ahmetcemsaydam@gmail.com>
2022-12-01 11:36:53 +0000

    couple of functions implemented
    couple of functions implemented however, it`s not done yet.

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,35 +2,43 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  return word.substring(0,1).toUpperCase() + word.substring(1, word.length);
+  return word.substring(0,1).toUpperCase() +   word.substring(1, word.length );
 }
 
 export function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstName.substring(0,1).toUpperCase() + "."  +lastName.substring(0,1).toUpperCase();
+  //  for more better runtime and efficiency one can use: 
+  //   return firstName.substring(0,1) + "."  +lastName.substring(0,1);
 }
 
 export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  return ( originalPrice + ( (vatRate/100) * originalPrice ) ) ; 
+
+  // not rounding the numbers properly
+  // take another look later
+  // do NOT forget
+
+  /*             */
 }
 
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  return ( originalPrice - ( (reduction/100)  * originalPrice).toFixed(2) );
 }
 
 export function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  return str.substr(Math.ceil(str.length / 2 - 1), str.length % 2 === 0 ? 2 : 1);
 }
 
 export function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.split('').reverse().join('');
 }
 
 export function reverseAllWords(words) {
