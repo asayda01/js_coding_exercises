69ed6500b94180c07970e5734680a1cc633384c6
Cem Saydam <ahmetcemsaydam@gmail.com>
2022-12-02 20:28:00 +0000

    completed the 4th exrcs and passed the tests

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -12,12 +12,16 @@ export function findNamesBeginningWith(names, char) {
 export function findVerbs(words) {
   if (!words) throw new Error("words is required");
   return words.filter(x => {return x.substring(0,3)== "to "});
+
+  // as a second approach
   //return words.filter(x => {return x.charAt(0)== "t" && x.charAt(1)== "o" && x.charAt(2)== " " });
 }
 
 export function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   return nums.filter(x => { return Number.isInteger(x) === true }) ;
+  
+  // as a second approach
   //return nums.filter(x => { return typeof x === 'number' && x % 1 === 0});
 }
 
@@ -34,11 +38,14 @@ export function getSquareRoots(nums) {
 export function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  //return sentences.filter( x => {return x.includes(str) }); // for case sensitive searches
   return sentences.filter( x => {return x.toUpperCase().includes(str.toUpperCase()) });
+
+  // for case sensitive searches
+  //return sentences.filter( x => {return x.includes(str) }); 
+  
 }
 
 export function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  return triangles.map( x => {return Math.max.apply(Math, x) } );
 }
