c4c0062a7e46dc942e72dd2b1cdd00dba2788dd7
Cem Saydam <ahmetcemsaydam@gmail.com>
2022-12-08 15:31:19 +0000

    completed the 5th exrcs and passed the tests.
    completed the 5th exercise and passed the tests.
    
    -fixed some typos

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -13,15 +13,11 @@ export const count1sand0s = (str) => {
   if (str.includes("0") === true ) {count_0 = str.match(/0/g).length};
   obj_1 = { 0 : count_0 , 1 : count_1 };
   return obj_1;
-
-  // could be improved *
-
 };
 
 export const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
   return Number(String(n).split('').reverse().join(''));
-
   // no leading zero(s) as it returns only numbers not strings
 };
 
@@ -29,9 +25,6 @@ export const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
   let sum_of_arrs = arrs.map(x =>  {return x.reduce((a, b) => a + b, 0)});
   return sum_of_arrs.reduce((k,m)=> k+m,0);
-
-  // could be improved * * *
-  // could be added an extra map function for an one-liner solution but runtime % complexity ? ? ? 
 };
 
 export const arrShift = (arr) => {
@@ -57,6 +50,5 @@ export const getWordFrequencies = (str) => {
      ++obj[element.toLowerCase().replace(/[&\/\\#,+()$~%.'":*?!<>{}]/g, '')] : 1;
   });
   return obj;
-
   // removes all non-letters characters
 };
